b3c11e495582500b40e890aec1cc0859b1c26210
Elsayed_Aboelmaaty <eng.sayed.gamal@gmail.com>
2023-03-27 20:59:07 +0200

    practicing Login with scrapy

--- quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/spiders/quotes_spider.py
@@ -1,13 +1,25 @@
 import scrapy
 from ..items import QuotetutorialItem
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
+    page_number = 2
     start_urls = [
-        'http://quotes.toscrape.com/'
+        'http://quotes.toscrape.com/login'
     ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata={
+            'csrf_token': token,
+            'username': 'mylongin name',
+            'password': 'ss'
+        }, callback=self.start_scraping)
+
+    def start_scraping(self, response):
+        open_in_browser(response)
         items = QuotetutorialItem()
         all_div_qoutes = response.css("div.quote")
 
@@ -21,7 +33,8 @@ class QuoteSpider(scrapy.Spider):
             items['tag'] = tag
 
             yield items
-        next_page = response.css('li.next a::attr(href)').get()
+        next_page = 'http://quotes.toscrape.com/page/' + str(QuoteSpider.page_number) + '/'
 
-        if next_page is not None:
-            yield response.follow(next_page, callback = self.parse)
+        if QuoteSpider.page_number < 11:
+            QuoteSpider.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
